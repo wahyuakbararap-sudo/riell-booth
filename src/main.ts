9f8d675e3075bd98e896d772baa311a87bfd0110
Gabriel <wahyuakbararap@gmail.com>
2026-06-07 19:19:55 +0700

    fix css import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router'
+import './style.css'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+createApp(App).use(router).mount('#app')
